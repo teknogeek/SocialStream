ecfaf61d2ded0ccdc0774f8320d1ffa82e02866b
Joel Margolis <jmargolis97@gmail.com>
2013-12-30 15:44:37 -0500

    Stuff

---
+++ src/com/ocelotworks/socialstream/Start.java
@@ -0,0 +1,9 @@
+package com.ocelotworks.socialstream;
+
+public class Start 
+{
+	public static void main(String args[])
+	{
+		mainWindow start = new mainWindow();;
+	}
+}
---
+++ src/com/ocelotworks/socialstream/mainWindow.java
@@ -0,0 +1,17 @@
+package com.ocelotworks.socialstream;
+
+import javax.swing.UIManager;
+import javax.swing.JFrame;
+
+public class mainWindow extends JFrame
+{
+		public void main(String[] args) {
+			start();
+		}
+		
+		public void start()
+		{
+			setSize(800, 600);
+			System.out.println("Runs");
+		}
+}
